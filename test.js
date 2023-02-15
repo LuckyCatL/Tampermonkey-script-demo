fc0e37ebee618efab64877b914ac186a363860ed
LuckyCatL <1373988036@qq.com>
2023-02-15 12:50:49 +0800

    修改描述

--- test.js
+++ test.js
@@ -2,7 +2,7 @@
 // @name         bilibili专栏复制去除后缀
 // @namespace    http://luckycat.ink/
 // @version      0.1
-// @description  bilibili专栏复制去除后缀
+// @description  bilibili专栏复制，去吃后缀作者链接信息
 // @author       LuckyCat
 // @match        https://www.bilibili.com/read/*
 // @icon         https://www.bilibili.com/favicon.ico
