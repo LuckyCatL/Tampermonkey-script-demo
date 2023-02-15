3c85859935d4cd83453c727ed341c4402edcd5cc
LuckyCatL <1373988036@qq.com>
2023-02-15 10:11:48 +0800

    1

--- test.js
+++ test.js
@@ -1,8 +1,8 @@
 // ==UserScript==
-// @name         bilibili复制修复
+// @name         测试
 // @namespace    http://luckycat.ink/
 // @version      0.1
-// @description  bilibili专栏复制修复
+// @description  test
 // @author       LuckyCat
 // @match        https://www.bilibili.com/read/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
