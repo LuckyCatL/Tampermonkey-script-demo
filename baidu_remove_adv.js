162e65952df96898e1b24e254a702e8f58698770
LuckyCatL <1373988036@qq.com>
2023-02-15 19:49:35 +0800

    去除清除计时器部分

--- baidu_remove_adv.js
+++ baidu_remove_adv.js
@@ -4,7 +4,6 @@
 // @version      0.1
 // @description  baidu百度去右侧广告
 // @author       LuckyCat
-// @match        https://www.baidu.com/
 // @match        https://www.baidu.com/*
 // @icon         https://www.baidu.com/favicon.ico
 // @license      MIT
@@ -14,20 +13,14 @@
 
 (function() {
     'use strict';
-    //let weburl=unsafeWindow.location.href
-    window.onload = function(){
         //let a = document.querySelectorAll('td')
         //console.log(a)
 
         //创建计时器
-        var del= setInterval(function(){
-            //console.log(a)
-            GM_addStyle('td{display:none !important}#s-top-left{display:none !important}');
+        var del = setInterval(function(){
+            //console.log('hello')
+            GM_addStyle('td{display:none !important}#s-top-left{display:none !important}.c-span6{display:none !important}');
         },500)
 
         //清除计时器
-        setTimeout(function(){
-             clearInterval(del)
-        },10000)
-    }
 })();
