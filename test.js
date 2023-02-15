28526c53b86e3dfad140e79d259f1d4133529c52
LuckyCatL <1373988036@qq.com>
2023-02-15 09:36:55 +0800

    1

--- test.js
+++ test.js
@@ -1,1 +1,15 @@
-console.log('2nd hello world')
+// ==UserScript==
+// @name         bilibili复制修复
+// @namespace    http://luckycat.ink/
+// @version      0.1
+// @description  bilibili专栏复制修复
+// @author       LuckyCat
+// @match        https://www.bilibili.com/read/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=bilibili.com
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    console.log(document.querySelectorAll('.unable-reprint'))
+})();
